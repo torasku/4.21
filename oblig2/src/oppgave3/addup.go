1015966ac3d0e4e12a85206fd3b13768f70712a3
torasku <tor.arve.skutlaberg@gmail.com>
2018-03-06 16:11:40 +0100

    first part oppg3

---
+++ oblig2/src/oppgave3/addup.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c := make(chan int)
+	go readInput(c)
+	time.Sleep(5 * 1e9)
+	go addUp(c)
+	time.Sleep(5 * 1e9)
+}
+
+func readInput(c chan int) {
+
+	var n1 int
+	var n2 int
+
+	fmt.Println("Enter num: ")
+	fmt.Scan(&n1)
+	fmt.Println("Enter num: ")
+	fmt.Scan(&n2)
+
+	c <- n1 //sender data via channel
+	c <- n2
+
+	res := <-c // mottar resultat fra channel
+	fmt.Println("Result: ", res)
+
+}
+
+func addUp(c chan int) {
+
+	n1, n2 := <-c, <-c // mottar data fra readInput()
+	res := (n1 + n2)
+
+	c <- res // sender resultat tilbake til readInput()
+
+}
